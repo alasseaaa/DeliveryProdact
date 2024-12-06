214b9c43de77a35c9678d79df4d56f9bef3bdae0
alasseaa <liza.chirkova.2004@gmail.com>
2024-12-06 12:41:15 +0300

    добавлен метод action(methods=['GET'], detail=False) статистика по товарам

--- deliveryfood/views.py
+++ deliveryfood/views.py
@@ -2,6 +2,10 @@ from django.http import HttpResponse
 import django_filters
 from rest_framework.filters import SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.decorators import action
+from django.db.models import Count, Q
+from rest_framework.response import Response
+from django.shortcuts import render, get_object_or_404
 from rest_framework import viewsets, serializers
 from .models import Product, Category, Profile, Address, Order, OrderedItem
 from .serializers import ProductSerializer, CategorySerializer, ProfileSerializer, AddressSerializer, OrderSerializer, OrderedItemSerializer
@@ -14,12 +18,60 @@ class ProductFilter(django_filters.FilterSet):
         model = Product
         fields = ["category", "description", "min_price", "max_price"]
 
+class ProductPriceSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Product
+            fields = ['price']
+            
+        def validate_price(self, value):
+            if value <0:
+                raise serializers.ValidationError("Цена не может быть меньше нуля.")
+            return value 
+
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends = [SearchFilter, DjangoFilterBackend]
     search_fields = ["name", "description", "category"]
     filterset_class = ProductFilter
+
+    @action(methods=["GET"], detail=False)
+    def statistics(self, request):
+       
+        all_count = self.get_queryset().count()
+        category_count = (
+            self.get_queryset().values("category__category_name").annotate(count=Count("id"))
+        )
+
+        return Response(
+            {
+                "Всего товаров": all_count,
+                "Статистика по категориям": category_count,
+            }
+        )
+
+
+    @action(methods=['POST'], detail=True)
+    def change_price(self, request, pk=None):
+
+        product = self.get_object()
+        serializer = ProductPriceSerializer(product, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Цена товара изменена."}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def get_serializer_class(self):
+
+        if self.action == 'change_price':
+            return ProductPriceSerializer
+        return super().get_serializer_class()
+   
+    # @action(methods=['GET'], detail=False)
+    # def sorted_by_price(self, request):
+    #     sorted_products = self.queryset.order_by('price')
+    #     serializer = self.get_serializer(sorted_products, many=True)
+    #     return Response(serializer.data)
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
