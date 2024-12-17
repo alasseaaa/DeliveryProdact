cf46ccd0e546dc5c88b7400531996642d6eaa759
alasseaa <liza.chirkova.2004@gmail.com>
2024-12-17 23:57:09 +0300

    подключен swagger

--- deliverysite/urls.py
+++ deliverysite/urls.py
@@ -19,7 +19,22 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from deliveryfood import views
+from rest_framework.permissions import AllowAny
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+SchemaView = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[AllowAny],
+)
 
 router = routers.DefaultRouter()
 router.register("products", views.ProductViewSet)
@@ -33,4 +48,5 @@ urlpatterns = [
     path("deliveryfood/", include("deliveryfood.urls")),
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
+    path('swagger/', SchemaView.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
