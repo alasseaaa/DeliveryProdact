836e7a3b7f9d4cc5a65c6245cce570da4ff522c1
alasseaa <liza.chirkova.2004@gmail.com>
2024-12-20 21:21:57 +0300

    настроен docker

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# Основа образа - Python 3.8
+FROM python:3.10.6
+
+RUN python -m pip install --upgrade pip
+WORKDIR /app
+# Установка необходимых пакетов
+COPY requirements.txt .
+RUN pip install -r requirements.txt
+
+
+# Копирование файлов проекта в контейнер
+COPY . .
+
+# Установка зависимостей Django
+# RUN python manage.py migrate
+
+# Запуск Django сервера
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000", "--noreload"]
--- deliveryfood/urls.py
+++ deliveryfood/urls.py
@@ -1,24 +1,11 @@
+from django.urls import include, path
 from rest_framework import routers
 from . import views
-from rest_framework.permissions import AllowAny
-from django.urls import path, include
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
 
-SchemaView = get_schema_view(
-    openapi.Info(
-        title="Snippets API",
-        default_version='v1',
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=[AllowAny],
-)
+# # app_name = "deliveryfood"
+
+
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path('swagger/', SchemaView.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
